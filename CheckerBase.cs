5a3c62fe8699f33bb56ef97bacdf4287b62205b2
APAC\EKG1COB <kennadi.vignesh@in.bosch.com>
2022-07-05 16:19:49 +0530

    updated code

---
+++ CheckerBase.cs
@@ -0,0 +1,45 @@
+﻿namespace Checker
+{
+
+    public abstract class CheckerBase
+    {
+        public float? MinValue { get; set; }
+
+        public float MaxValue { get; set; }
+
+        protected CheckerBase(float? minValue, float maxValue)
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+
+        public bool IsValueWithinRange(float value)
+        {
+            return (value < MinValue || value > MaxValue);
+        }
+    }
+
+    public class TemperatureChecker : CheckerBase
+    {
+        public TemperatureChecker(float minValue, float maxValue) : base(minValue, maxValue)
+        {
+
+        }
+    }
+
+    public class SocChecker : CheckerBase
+    {
+        public SocChecker(float minValue, float maxValue) : base(minValue, maxValue)
+        {
+
+        }
+    }
+
+    public class ChargeRateChecker : CheckerBase
+    {
+        public ChargeRateChecker(float? minValue, float maxValue) : base(minValue, maxValue)
+        {
+
+        }
+    }
+}
--- checker.cs
+++ checker.cs
@@ -1,38 +1,31 @@
 using System;
-using System.Diagnostics;
 
-class Checker
+namespace Checker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
+    internal static class Checker
+    {
+        static bool BatteryIsOk(float temperature, float soc, float chargeRate)
+        {
+            return !(new TemperatureChecker(0, 45).IsValueWithinRange(temperature) || new SocChecker(20, 80).IsValueWithinRange(soc) || new ChargeRateChecker(null, 0.8f).IsValueWithinRange(chargeRate));
         }
-        return true;
+
+        static void ExpectedResult(bool expression, bool expectedResult)
+        {
+            if (expression && !expectedResult)
+            {
+                Console.WriteLine("Expected" + expectedResult.ToString() + " , but got" + !expectedResult);
+                Environment.Exit(1);
+            }
+        }
+
+        static int Main()
+        {
+            ExpectedResult(BatteryIsOk(25, 70, 0.7f), true);
+            ExpectedResult(BatteryIsOk(50, 85, 0.0f), false);
+            Console.WriteLine("All ok");
+            return 0;
+        }
     }
 
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }
-    static int Main() {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
-        Console.WriteLine("All ok");
-        return 0;
-    }
+
 }
