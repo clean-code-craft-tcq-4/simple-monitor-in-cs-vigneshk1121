7da47fddf333adfe238c17e37bf90b9bfd181dc1
APAC\EKG1COB <kennadi.vignesh@in.bosch.com>
2022-07-05 18:42:31 +0530

    fixed build issues

--- CheckerBase.cs
+++ CheckerBase.cs
@@ -17,4 +17,59 @@
             return (value < MinValue || value > MaxValue);
         }
     }
+
+    public class TemperatureChecker : CheckerBase
+    {
+        public TemperatureChecker() : base(Limits.MIN_TEMPERATURE, Limits.MAX_TEMPERATURE)
+        {
+
+        }
+
+        public new bool IsValueWithinRange(float value)
+        {
+            bool val = base.IsValueWithinRange(value);
+            if (val)
+            {
+                Console.WriteLine("Temperature is out of range!");
+            }
+            return val;
+        }
+    }
+
+    public class SocChecker : CheckerBase
+    {
+        public SocChecker() : base(Limits.MIN_SOC, Limits.MAX_SOC)
+        {
+
+        }
+
+        public new bool IsValueWithinRange(float value)
+        {
+            bool val = base.IsValueWithinRange(value);
+            if (val)
+            {
+                Console.WriteLine("State of Charge is out of range!");
+            }
+            return val;
+        }
+    }
+
+    public class ChargeRateChecker
+    {
+        public ChargeRateChecker()
+        {
+
+        }
+
+        public bool IsValueWithinRange(float value)
+        {
+            var result = (value > Limits.PERMISSIBLE_CHARGE_RATE);
+            if (result)
+            {
+                Console.WriteLine("Charge Rate is out of range!");
+            }
+
+            return result;
+        }
+    }
 }
